1984df292267aec0cd5c8a13a1f46e2078eda595
Akshat Kumar Sinha <95994418+AkshatKumarSinha9327@users.noreply.github.com>
2022-07-14 11:50:11 +0530

    Update Half_diamond_star_pattern.c

--- Half_diamond_star_pattern.c
+++ Half_diamond_star_pattern.c
@@ -9,15 +9,13 @@ int main(){
             for(j=1;j<=i;j++){
                 printf("*");
             }
-            printf("
-");
+            printf("\n");
         }
         for (i=n-1;i>=1;i--){
             for(j=i;j>=1;j--){
                 printf("*");
             }
-            printf("
-");
+            printf("\n");
         }
         
     }
